18ba5c6b433d00cf2713579c49bde203447500fb
Done_Solutions <getit@done.to>
2016-08-18 17:02:06 +0530

    cosmetic changes

--- app/src/main/java/com/shock/demoapp/ui/list/view/ListActivityDetailFragment.java
+++ app/src/main/java/com/shock/demoapp/ui/list/view/ListActivityDetailFragment.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.AppCompatImageView;
 import android.support.v7.widget.AppCompatTextView;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.LinearLayoutCompat;
 import android.text.format.DateFormat;
 import android.util.Log;
@@ -127,6 +128,9 @@ public class ListActivityDetailFragment extends AppBaseFragment implements View.
 
     @BindView(R.id.tv_prod_tnc_label)
     AppCompatTextView tvProdTncLabel;
+
+    @BindView(R.id.cv_prod_tnc)
+    CardView cvProdTnc;
 
     @BindView(R.id.ll_prod_tnc)
     LinearLayoutCompat llProdTnc;
@@ -285,6 +289,7 @@ public class ListActivityDetailFragment extends AppBaseFragment implements View.
             textView.setText(s);
             llProdTnc.addView(compat);
         }
+        cvProdTnc.setVisibility(View.VISIBLE);
     }
 
     private void setGridCellAdapterToDate(int month, int year) {
